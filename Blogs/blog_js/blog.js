bb2de4a20134da82713afa5c2881fe8d484f070f
Serj <sergeykoz937@gmail.com>
2021-01-29 19:07:55 +0200

    blog

---
+++ Blogs/blog_js/blog.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $.getJSON('blog-json/article.json', function (json) {
+        $('.blog_post').pagination({
+            dataSource: json,
+            pageSize: 6,
+            prevText: 'First',
+            nextText: 'Next',
+            callback: function(data, pagination) {
+                $('#post_article').empty();
+                $.each(data, function (i, f) {
+                    $('#post_article ').append(`
+                    <div class="head_article">
+                        <div class="article_img">
+                            <a href="#"><img src="${f.image}" alt=""></a>
+                        </div>
+                        <div class="article_main">
+                            <div class="article_text">
+                                <span>${f.date}</span>
+                                <h3>${f.title}</h3>
+                                <p>${f.text}</p>
+                            </div>
+                            <div class="blog_link">
+                                <a href="#" target="_blank">Google</a>
+                                <a href="#" target="_blank">Trending</a>
+                                <a href="#" target="_blank">New</a>
+                            </div>
+                        </div>
+                    </div>`
+                    );
+                    
+                });
+            }
+        });
+    });
+});
+
+
+
+
+
+
+
+
+    
